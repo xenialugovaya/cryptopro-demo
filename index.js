fd9e2051da0c5d65e69b3f6ede0519667d16083d
Ksenia Lugovaya <lugovaya.ka@esphere.ru>
2021-03-19 10:28:06 +0300

    Updates

---
+++ index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+ReactDOM.render(React.createElement("div", null, "Hello"), document.getElementById('root'));
+//# sourceMappingURL=index.js.map
